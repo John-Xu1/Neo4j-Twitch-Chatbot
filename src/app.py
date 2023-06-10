ba818d40961a26688755b03690b1929ad529998e
John-Xu1 <XuJohn400@gmail.com>
2023-06-10 18:11:00 -0400

    Chat frontent

--- src/app.py
+++ src/app.py
@@ -1,4 +1,34 @@
-import streamlit_chat
+import streamlit as st
 from streamlit_chat import message
 
-message('test')
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+
+if 'userInput' not in st.session_state:
+    st.session_state['userInput'] = []
+
+if 'input' not in st.session_state:
+    st.session_state['input'] = ''
+
+
+def getText():
+    input = st.text_input("What can we help with?",
+                          value=st.session_state.input)
+    return input
+
+
+placeholder = st.empty()
+inputIdx = 0
+userInput = getText()
+if userInput:
+    st.session_state.userInput.append(userInput)
+    st.session_state.generated.append("test answer")
+
+with placeholder.container():
+    if st.session_state['generated']:
+        size = len(st.session_state['generated'])
+        for i in range(0, size):
+            message(st.session_state['userInput'][i],
+                    is_user=True, key='user message ' + str(i))
+            message(st.session_state["generated"]
+                    [i], key='bot message ' + str(i))
